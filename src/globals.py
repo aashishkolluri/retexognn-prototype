888fbce460c38ef9cc88513f79f9d35b5b5e4b7d
Sarthak <f20190112@pilani.bits-pilani.ac.in>
2023-05-11 07:30:13 +0000

    adding requirements file

--- src/globals.py
+++ src/globals.py
@@ -7,7 +7,7 @@ class MyGlobals(object):
     num_seeds = 1
     sample_seed = 42
     cuda_id = 0
-    hidden_size = 16
+    hidden_size = 256
     num_hidden = 2
     attn_heads = 8
 
@@ -15,7 +15,7 @@ class MyGlobals(object):
     lr = 0.05
     num_epochs = 500
     save_epoch = 100
-    dropout = 0.1
+    dropout = 0.0
     weight_decay = 5e-4
     optimizer = "sgd"
     momentum = 0.9
